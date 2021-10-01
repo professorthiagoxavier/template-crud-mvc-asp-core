4e361ec4d245a75080c7b27c2dcd21e3f350f13f
THiago Xavier <xavier7132@gmail.com>
2021-10-01 18:05:32 -0300

    template'

---
+++ Controllers/AtividadesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class AtividadesController : Controller
+    {
+        private readonly WebApplication1Context _context;
+
+        public AtividadesController(WebApplication1Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Atividades
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Atividade.ToListAsync());
+        }
+
+        // GET: Atividades/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var atividade = await _context.Atividade
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (atividade == null)
+            {
+                return NotFound();
+            }
+
+            return View(atividade);
+        }
+
+        // GET: Atividades/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Atividades/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,rm,nome,urlGitHub,numAtividade")] Atividade atividade)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(atividade);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(atividade);
+        }
+
+        // GET: Atividades/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var atividade = await _context.Atividade.FindAsync(id);
+            if (atividade == null)
+            {
+                return NotFound();
+            }
+            return View(atividade);
+        }
+
+        // POST: Atividades/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,rm,nome,urlGitHub,numAtividade")] Atividade atividade)
+        {
+            if (id != atividade.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(atividade);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AtividadeExists(atividade.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(atividade);
+        }
+
+        // GET: Atividades/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var atividade = await _context.Atividade
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (atividade == null)
+            {
+                return NotFound();
+            }
+
+            return View(atividade);
+        }
+
+        // POST: Atividades/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var atividade = await _context.Atividade.FindAsync(id);
+            _context.Atividade.Remove(atividade);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AtividadeExists(int id)
+        {
+            return _context.Atividade.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/Atividade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Atividade
+    {
+        public int Id { get; set; }
+        public int rm { get; set; }
+        public string nome { get; set; }
+        public string urlGitHub { get; set; }
+        public int numAtividade { get; set; }
+    }
+}
